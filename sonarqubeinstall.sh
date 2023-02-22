70a7c39ab90e82ff5735f3b0db34528097992328
Boozley <100140152+Boozley@users.noreply.github.com>
2023-02-22 01:53:38 -0500

    Update sonarqubeinstall.sh

--- sonarqubeinstall.sh
+++ sonarqubeinstall.sh
@@ -18,15 +18,15 @@ cd /opt
 
 sudo yum install wget -y
 
-sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.0.65466.zip
 
 sudo yum install unzip -y
 
-sudo unzip /opt/sonarqube-9.3.0.51899.zip
+sudo unzip /opt/sonarqube-9.9.0.65466.zip
 
-sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+sudo chown -R vagrant:vagrant /opt/sonarqube-9.9.0.65466
 
-cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
+cd /opt/sonarqube-9.9.0.65466/bin/linux-x86-64
 
 ./sonar.sh start
 
